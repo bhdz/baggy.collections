88551bc9b5eec006340c91ec2d21edf676a3ae83
someone <borko.boyanov@gmail.com>
2016-12-15 05:30:26 +0200

    baggs
    baggo { sh1t }; +1; Good..
    bagg = ???????????????FFFFUUUUCK OSTANA NA DRUGIA DESKTP... ahahahah sorry empty bag o sh1t

---
+++ modules/baggy/baggy_lib.c
@@ -0,0 +1,15 @@
+/** HAHA!! LOL !!! FIRST THING FIRST!?? */
+/** MACRO:: HERE PLS DR.RACKET HELP MAAAAAN */
+#define _V__20 "Version:2.0v"
+
+#define _BHDZ_ _V__20
+
+int main(int a, char *argc[ _((. (8_). _ . 0 .)) ] ) {
+  printf("Hello World!\n");
+  printf("This is an example; of {a:1} [:library]\n"
+  printf("THAT; WILL;HOST; A MINIMUM; OF TYPES; OF COLECTIONS; That...; HAHA {{\n");
+  printf("  ~say('<good-buye>, &from, &to, *env)!...");
+  printf("}}; HAHA; +Sirl you win Again, We Shall see you On Other Battlfields!!");
+  
+  return 0;
+}
